0e425b73f49ff16d29d850a5f8b3161e5163b3f5
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-04-08 21:11:27 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  function hasTargetSum(array, target) {
+    const numSet = new Set();
+
+    for (let num of array) {
+        const complement = target - num;
+        if (numSet.has(complement)) {
+            return true;
+        }
+        numSet.add(num);
+    }
+
+    return false;
 }
 
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // Should return true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); // Should return true
+console.log(hasTargetSum([1, 2, 5], 4)); // Should return false
+console.log(hasTargetSum([-7, 10, 4, 8], 3)); // Should return true
+console.log(hasTargetSum([1, 2, 3, 4], 5)); // Should return true
+console.log(hasTargetSum([2, 2, 3, 3], 4)); // Should return true
+console.log(hasTargetSum([4], 4)); // Should return false
+
+
+
 /* 
-  Write the Big O time complexity of your function here
+  The time complexity of the hasTargetSum function is O(n), where n is the length of the input array. 
 */
 
 /* 
-  Add your pseudocode here
+  FUNCTION hasTargetSum(array, target):
+    numSet = new Set()
+    FOR EACH num IN array:
+        complement = target - num
+        IF numSet contains complement:
+            RETURN true
+        numSet.add(num)
+    RETURN false
+
 */
 
 /*
-  Add written explanation of your solution here
+  The function efficiently uses a Set data structure to store seen numbers and check for the existence of complements in constant time. It iterates through the array only once, resulting in a time complexity of O(n), where n is the length of the input array. This approach provides an efficient solution for finding a pair of numbers with a target sum in an array.
 */
 
 // You can run `node index.js` to view these console logs
